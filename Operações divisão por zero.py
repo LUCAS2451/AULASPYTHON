d1f7a6fc1a9ce533af22dbb01460574a4f9bb2e1
LUCAS2451 <lf296890@gmail.com>
2025-06-07 11:29:09 -0300

    Add files via upload

---
+++ Operações divisão por zero.py
@@ -0,0 +1,17 @@
+def main():
+  num1 = int(input("digite un numero: "))
+  num2 = int(input("digite outro numero: "))
+
+  soma = num1 + num2
+  sub = num1 - num2
+  mult = num1 * num2
+  if num2 == 0:
+   div = "impossivel dividir por zero"
+  else:
+   div = num1 / num2
+  print("a soma é: ", num1," + ",num2," = ", soma)
+  print("a soma é:",num1," - ",num2," = ",sub)
+  print("a soma é:",num1," * ",num2," = ",mult)
+  print("a soma é:",num1," / ", num2," = ",div)
+  return 0
+main()
